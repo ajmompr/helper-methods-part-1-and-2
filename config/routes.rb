afe185db047d08e1e8d686eafe382ab48eb6d233
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-10-16 21:55:30 +0000

    added names to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Read
-  root "movies#index"
+  root "movies#index", as: :movies # gives us movies_url (returns string containing URL) & movies_path (which returns path string)
 
   # Routes for the Movie resource:
 
@@ -8,15 +8,15 @@ Rails.application.routes.draw do
   post "/movies" => "movies#create"
 
   # NEW
-  get "/movies/new" => "movies#new"
+  get "/movies/new" => "movies#new", as: :new_movie #new_movie_url & new_movie_path
 
   # READ
   get "/movies" => "movies#index"
-  get "movies/:id" => "movies#show"
+  get "movies/:id" => "movies#show", as: :movie #show page is typically singular; movie_path() --> expects an argument to populate :id segment
 
   #Update 
   patch "/movies/:id" => "movies#update"
-  get "movies/:id/edit" => "movies#edit"
+  get "movies/:id/edit" => "movies#edit", as: :edit_movie # edit_movie_path()  --> expects an argument to populate :id segment
 
   #Delete
   delete "/movies/:id" => "movies#destroy"
