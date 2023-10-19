89c1d54ac2ce93da1a1fb43e1f12f133c2e4e174
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-10-19 15:36:59 +0000

    added new, index, create, show, edit, update, destroy actoins to directors_controller

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,22 +1,64 @@
 class DirectorsController < ApplicationController
   def new
+    @director = Director.new
   end
 
   def index
+    matching_directors = Director.all
+
+    @directors = matching_directors.order( created_at: :desc)
+
+    respond_to do |format|
+      format.json do
+        render json: @movies
+      end
+
+      format.html do
+        
+      end
+    end
   end
 
   def show
+    @director = Director.find(params.fetch(:id))
   end
 
   def create
+    director_attributes = params.require(:director).permit(:name, :bio, :dob)
+    @director = Director.new(director_attributes)
+
+    if @director.valid?
+      @director.save
+      redirect_to directors_url, notice: "Director created successfully."
+    else
+      render template: "directors/new"
+    end
   end
 
   def edit
+    @director = Director.find(params.fetch(:id))
   end
 
   def update
+    @director =  Director.find(params.fetch(:id))
+    director_attributes = params.require(:director).permit(:name, :bio, :dob)
+
+    @director.update(director_attributes)
+
+    if @director.valid?
+      @director.save
+      redirect_to director_url(@director), notice: "Director updated successfully."
+    else
+      redirect_to director_url(@director), alert: "Director failed to update successfully"
+  
+    end
   end
 
   def destroy
+    @director = Director.find(params.fetch(:id))
+
+    @director.destroy
+
+    redirect_to directors_url, notice: "Director deleted successfully."
   end
 end
